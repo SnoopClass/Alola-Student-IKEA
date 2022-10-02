822a35150d10af8cc1c2f45bbdbb70cd25af306b
compuTasha <mijookim.tasha@gmail.com>
2022-10-03 02:06:09 +0900

    [Add] 탭바 border 추가 (#2)

--- IKEA/IKEA/Sources/ViewControllers/TabBarViewController.swift
+++ IKEA/IKEA/Sources/ViewControllers/TabBarViewController.swift
@@ -8,22 +8,28 @@
 import UIKit
 
 class TabBarViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.selectedIndex = 1
-        // Do any additional setup after loading the view.
+        
+        setBorderForTabBar()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setBorderForTabBar() {
+        self.tabBar.layer.borderWidth = 0.4
+        self.tabBar.layer.borderColor = UIColor.lightGray.cgColor
+        self.tabBar.clipsToBounds = true
     }
-    */
-
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
