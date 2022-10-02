8c57204836fe236f0e63ca071318f982437fefd4
compuTasha <mijookim.tasha@gmail.com>
2022-10-03 00:40:30 +0900

    [Add] 처음 실행되는 탭 변경 (#2)
    두번째 탭인 검색 화면이 가장 먼저 실행되도록 변경

--- IKEA/IKEA/Sources/ViewControllers/TabBarViewController.swift
+++ IKEA/IKEA/Sources/ViewControllers/TabBarViewController.swift
@@ -11,6 +11,7 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.selectedIndex = 1
         // Do any additional setup after loading the view.
     }
     
