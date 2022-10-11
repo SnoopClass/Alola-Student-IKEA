97b2a74f97480eb331be2fa78d1758ac4b33574b
compuTasha <mijookim.tasha@gmail.com>
2022-10-12 07:59:48 +0900

    [Add] 네비게이션바 스크롤 시 숨기기 (#1)

---
+++ IKEA/IKEA/Sources/ViewControllers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  IKEA
+//
+//  Created by Mijoo Kim on 2022/10/12.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationController?.hidesBarsOnSwipe = true
+  }
+
+
+}
+
