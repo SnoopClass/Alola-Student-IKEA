7bc825a683ab82a0ded15b9aaa2017f486586b7d
compuTasha <mijookim.tasha@gmail.com>
2022-10-17 07:21:49 +0900

    [Add] 서치바 UI 커스텀 (#1)

---
+++ IKEA/IKEA/Sources/Extensions/UISearchBar+.swift
@@ -0,0 +1,8 @@
+//
+//  UISearchBar+.swift
+//  IKEA
+//
+//  Created by Mijoo Kim on 2022/10/13.
+//
+
+import Foundation
--- IKEA/IKEA/Sources/ViewControllers/RecentKeywordViewController.swift
+++ IKEA/IKEA/Sources/ViewControllers/RecentKeywordViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RecentKeywordViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
